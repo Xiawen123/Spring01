c3a9b39042bf9616d40006f140c1e7e4f1b803a8
xjx <774737421@qq.com>
2019-11-23 18:38:11 +0800

    000

---
+++ src/main/java/com/test/AAA.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import com.sun.org.apache.xml.internal.res.XMLErrorResources_tr;
+
+/**
+ * @author: XJX
+ * @program: Spring01
+ * @description: TODO
+ * @Date: 2019/11/23 18:36
+ **/
+public class AAA {
+    public static void main(String[] args) {
+        System.out.println("000000");
+    }
+}
